bb3b0c457b45df654bf6abed4e350361c85fa28d
minju77 <imminju7@gmail.com>
2026-07-19 12:05:14 +0900

    test: add test runner script

---
+++ scripts/test_all.bat
@@ -0,0 +1,78 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+cd /d "%~dp0\.."
+
+echo.
+echo ========================================
+echo Zero-CPU Test Suite
+echo ========================================
+echo.
+
+echo [1/6] Building project...
+cmake --build build
+if errorlevel 1 goto fail
+
+set "ZERO_CLI=build\Debug\zero_cli.exe"
+
+if not exist "%ZERO_CLI%" (
+    set "ZERO_CLI=build\Release\zero_cli.exe"
+)
+
+if not exist "%ZERO_CLI%" (
+    echo.
+    echo ERROR: zero_cli.exe not found.
+    echo Tried:
+    echo   build\Debug\zero_cli.exe
+    echo   build\Release\zero_cli.exe
+    goto fail
+)
+
+echo.
+echo Using CLI:
+echo   %ZERO_CLI%
+echo.
+
+echo [2/6] Running ALU unit test...
+"%ZERO_CLI%" alu-test
+if errorlevel 1 goto fail
+
+echo.
+echo [3/6] Running binary format round-trip test...
+"%ZERO_CLI%" binary-test
+if errorlevel 1 goto fail
+
+echo.
+echo [4/6] Assembling function_call.zasm...
+"%ZERO_CLI%" assemble examples\function_call.zasm examples\function_call.zbin
+if errorlevel 1 goto fail
+
+echo.
+echo [5/6] Running function_call.zbin...
+"%ZERO_CLI%" run-binary examples\function_call.zbin --watch 100 2048
+if errorlevel 1 goto fail
+
+echo.
+echo [6/6] Assembling and running alu_flags.zasm...
+"%ZERO_CLI%" assemble examples\alu_flags.zasm examples\alu_flags.zbin
+if errorlevel 1 goto fail
+
+"%ZERO_CLI%" run-binary examples\alu_flags.zbin --watch 120 128 136 144 152 160 168 200
+if errorlevel 1 goto fail
+
+echo.
+echo ========================================
+echo All Zero-CPU tests passed.
+echo ========================================
+echo.
+
+exit /b 0
+
+:fail
+echo.
+echo ========================================
+echo Zero-CPU tests failed.
+echo ========================================
+echo.
+
+exit /b 1
